2ae8976682008b7c1c732f6bad8538fa2ad49c9a
Benjamin Lewis <23inhouse@gmail.com>
2022-10-18 21:58:42 +0200

    Add ios app boilerplate

---
+++ Poker/Poker/PokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerApp.swift
+//  Poker
+//
+//  Created by Benjamin Lewis on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
